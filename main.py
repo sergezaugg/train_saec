13975e630eac239fdad993741837bd1989bbb478
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-30 16:47:32 +0200

    edited main.py

--- main.py
+++ main.py
@@ -6,13 +6,10 @@
 import torch
 from src.train_saec import MakeColdAutoencoders, AutoencoderTrain, EvaluateReconstruction
 
-
-torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 cold_dir = "dev/outp/cold_models"
 hot_dir = "dev/outp/hot_models"
-
 dat_tra_dir = "dev/data/train/images"
 dat_tes_dir = "dev/data/test/images" 
 
@@ -21,38 +18,26 @@ mca = MakeColdAutoencoders(dir_cold_models = cold_dir)
 mod_arch = mca.make()
 
 # Either, initialize a AEC-trainer with a naive model 
-at = AutoencoderTrain(
-                      data_gen = 'baseline', 
-                    #   data_gen = 'daugm_denoise', 
-                    #   data_gen = 'daugm_only', 
-                      # data_gen = 'denoise_only', 
-                      dir_cold_models = cold_dir, 
-                      dir_hot_models = hot_dir,
-                      dir_train_data = dat_tra_dir, 
-                      dir_test_data = dat_tes_dir,
-                      hot_start = False, 
-                      model_tag = "GenBTP16_CH0256", 
-                      device = device
-                      )
-
-# Or, initialize a AEC-trainer with a pre-trained model
-at = AutoencoderTrain(data_gen = 'daugm_denoise', 
-                      dir_cold_models = cold_dir, 
-                      dir_hot_models = hot_dir,
-                      dir_train_data = dat_tra_dir, 
-                      dir_test_data = dat_tes_dir,
-                      hot_start = True, 
-                      model_tag = "20250630_112654", 
-                      device = device)
+at = AutoencoderTrain(data_gen = 'daugm_denoise', dir_cold_models = cold_dir, dir_hot_models = hot_dir,
+						dir_train_data = dat_tra_dir, dir_test_data = dat_tes_dir,
+						hot_start = False, model_tag = "GenBTP16_CH0256", device = device
+						)
 
 # Directly check data augmentation
 at.make_data_augment_examples().show()
+
 # Start training (.pth files will be saved to disk)
-at.train_autoencoder(n_epochs = 3, batch_size_tr = 8, batch_size_te = 32, devel = True)
+_, _, tstmp01 = at.train_autoencoder(n_epochs = 3, batch_size_tr = 8, batch_size_te = 32, devel = True)
+
+# Or, initialize a AEC-trainer with a pre-trained model
+at = AutoencoderTrain(data_gen = 'daugm_denoise', dir_cold_models = cold_dir, dir_hot_models = hot_dir,
+						dir_train_data = dat_tra_dir, dir_test_data = dat_tes_dir,
+						hot_start = True, model_tag = tstmp01, device = device
+                        )
+
+_, _, tstmp02 = at.train_autoencoder(n_epochs = 3, batch_size_tr = 8, batch_size_te = 32, devel = True)
 
 # EvaluateReconstruction
 er = EvaluateReconstruction(dir_hot_models = hot_dir, device = device)
-er.evaluate_reconstruction_on_examples(
-    path_images = dat_tes_dir, 
-    time_stamp_model = "20250630_112752", n_images = 64, shuffle = False).show()
+er.evaluate_reconstruction_on_examples(path_images = dat_tes_dir, time_stamp_model = tstmp02, n_images = 32, shuffle = False).show()
 
